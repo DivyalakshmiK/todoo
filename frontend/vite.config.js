94ccc7c3053f36d0973158d85540905438a4ccfd
Divyalakshmi <divyalakshmikumar243@gmail.com>
2025-08-20 10:27:32 +0530

    deploying

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/todoo/', // Add this for GitHub Pages
   plugins: [react(),
     tailwindcss(),
   ],
